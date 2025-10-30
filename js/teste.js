d63005604828c39a6fa882961c1b50f0d27e29d6
isafernandesr <isadora.r@escolar.ifrn.edu.br>
2025-10-30 15:15:01 -0300

    alterações

--- js/teste.js
+++ js/teste.js
@@ -1,1 +1,8 @@
-document.getElementById("video1").play();
+window.addEventListener("scroll", function() {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
